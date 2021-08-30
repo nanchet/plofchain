873d723cc59d184bab8037a7bd89a5db43ce7346
nanchet <nanchet@gmail.com>
2021-08-30 21:37:29 +0200

    uso método para calcular el importe y controlo si el token es un proxy

--- public/dist/app.js
+++ public/dist/app.js
@@ -6,7 +6,6 @@ let app = function () {
     let contract;
     let token;
     let pool;
-    let divisor = 1000000000000000000;
     let zeroAddress = '0x0000000000000000000000000000000000000000';
 
     let connectButton = document.getElementById('connect');
@@ -57,13 +56,26 @@ let app = function () {
         pool.info = await contract.methods.poolInfo(poolId).call();
 
         poolDataContent.innerText =
-            'Deposited: ' + pool.amount / divisor +
-            ' Rewards: ' + pool.rewardDebt / divisor;
+            'Deposited: ' + web3.utils.fromWei(pool.amount) +
+            ' Rewards: ' + web3.utils.fromWei(pool.rewardDebt);
 
         getRewardsButton.onclick = getRewards;
         depositButton.onclick = deposit;
 
         poolInfo.style.display = 'initial';
+    }
+
+    let getToken = async function (abi, contractAddress) {
+        let contract = await new web3.eth.Contract(abi, contractAddress);
+
+        if (contract.methods.hasOwnProperty('implementation')) {
+            let impAddress = await contract.methods.implementation().call();
+            let impAbi = await fetchAbi(impAddress);
+
+            contract = await new web3.eth.Contract(impAbi, contractAddress);
+        }
+
+        return contract;
     }
 
     let fetchAbi = async function (contractAddress) {
@@ -85,15 +97,15 @@ let app = function () {
         let poolId = document.getElementById('poolId').value;
 
         let abi = await fetchAbi(pool.info.lpToken);
-        token = await new web3.eth.Contract(abi, pool.info.lpToken);
+        token = await getToken(abi, pool.info.lpToken);
 
         let balance = await token.methods.balanceOf(from).call();
         let symbol = await token.methods.symbol().call();
 
-        let amount = window.prompt(symbol + ' amount?', balance / divisor);
+        let amount = window.prompt(symbol + ' amount?', web3.utils.fromWei(balance));
 
         if (amount != null && amount != '') {
-            let response = await contract.methods.deposit(poolId, BigInt(amount * divisor), zeroAddress).send({ from });
+            let response = await contract.methods.deposit(poolId, web3.utils.toWei(amount), zeroAddress).send({ from });
 
             return response;
         }
